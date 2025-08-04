add606ecb77fae3e9be397f7d2f1a15425c44a26
Dave <davidglogowski2003@gmail.com>
2025-08-05 00:01:18 +0200

    Add stream filtering for non forked repositories and add branch retrieval method

--- src/main/java/com/atipera/zadanie/githubrepofetcher/service/GithubRepoFetcherService.java
+++ src/main/java/com/atipera/zadanie/githubrepofetcher/service/GithubRepoFetcherService.java
@@ -1,35 +1,70 @@
 package com.atipera.zadanie.githubrepofetcher.service;
 
+import com.atipera.zadanie.githubrepofetcher.model.Branch;
+import com.atipera.zadanie.githubrepofetcher.model.GithubRepository;
+import com.atipera.zadanie.githubrepofetcher.response.RepositoryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GithubRepoFetcherService {
 
     private final RestTemplate restTemplate;
 
+    @Value("${github.api.url}")
+    private String githubApiUrl;
+
     @Autowired
     public GithubRepoFetcherService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public void fetchUsersRepositories (String username) {
+    public List<RepositoryResponse> fetchUsersRepositories (String username) {
 
-        String path = "/users/" +  username + "/repos?type=all";
+        String url = UriComponentsBuilder.fromUriString(githubApiUrl)
+                .path("/users/{username}/repos")
+                .queryParam("type", "all")
+                .buildAndExpand(username)
+                .toUriString();
 
         try {
 
+            GithubRepository[] repositories = restTemplate.getForObject(url, GithubRepository[].class);
 
-
+            return Arrays.stream(repositories)
+                    .filter(repository -> !repository.isForked())
+                    .map(repo -> {
+                        List<Branch> branches = getRepositoryBranches(username, repo.name());
+                        return new RepositoryResponse(repo.name(), repo.owner().login(), branches);
+                    })
+                    .collect(Collectors.toList());
         } catch (HttpClientErrorException.NotFound e) {
             throw new RuntimeException(e.getMessage());
         }
 
+    }
 
+    private List<Branch> getRepositoryBranches (String username, String repositoryName) {
 
+        String url = UriComponentsBuilder.fromUriString(githubApiUrl)
+                .path("/repos/{username}/{repositoryName}/branches")
+                .queryParam("type", "all")
+                .buildAndExpand(username)
+                .toUriString();
+
+        Branch[] branches = restTemplate.getForObject(url, Branch[].class);
+
+        return Arrays.stream(branches)
+                .map(branch -> new Branch(branch.name(), branch.headCommit()))
+                .collect(Collectors.toList());
 
     }
 
